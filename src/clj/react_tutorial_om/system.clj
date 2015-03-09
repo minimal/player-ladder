05aabdfa47f30bdac8f3f6c8cab3a1ba837002c2
Chris McDevitt <christophermcdevitt@gmail.com>
2015-03-09 22:03:12 +0000

    take port on cmdline

--- src/clj/react_tutorial_om/system.clj
+++ src/clj/react_tutorial_om/system.clj
@@ -3,15 +3,16 @@
             [com.stuartsierra.component :as component])
   (:gen-class))
 
-(defn make-system [{:keys [is-dev? db-file slack-url]}]
+(defn make-system [{:keys [is-dev? db-file slack-url port]}]
   (component/system-map
-   :webserver (core/new-webserver {:ring {:port 3000 :join? false}
+   :webserver (core/new-webserver {:ring {:port (or port 3000) :join? false}
                                    :slack-url slack-url
                                    :db-file db-file
                                    :is-dev? is-dev?})))
 
 (defn -main
-  [file & [slack-url]]
+  [file & [port slack-url]]
   (component/start (make-system {:is-dev? false
                                  :db-file file
+                                 :port (Integer/parseInt port)
                                  :slack-url slack-url})))
