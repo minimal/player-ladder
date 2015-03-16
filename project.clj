decc0bd792ff93111b0870d99f0e5cacb714e38f
Chris McDevitt <christophermcdevitt@gmail.com>
2015-03-16 23:49:14 +0000

    upgrade deps
    no need for lein-figwheel anymore
    don't use inspect yet
    update macro import usage for cljs

--- project.clj
+++ project.clj
@@ -6,14 +6,13 @@
   :dependencies [[clj-time "0.9.0"]
                  [clj-http "1.0.1"]
                  [cljs-http "0.1.27"]
-                 [com.matthiasnehlsen/inspect "0.1.11"]
                  [com.stuartsierra/component "0.2.3"]
                  [compojure "1.3.2"]
                  [environ "1.0.0"]
                  [metosin/compojure-api "0.18.0" :exclude [ring-middleware-format prismatic/plumbing]]
                  [org.omcljs/om "0.8.8"]
                  [org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-3119"]
+                 [org.clojure/clojurescript "0.0-3123"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.reader "0.8.16"]
                  [prismatic/om-tools "0.3.10"]
@@ -24,7 +23,7 @@
                  [ring/ring-core "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
                  [sablono "0.3.4"]
-                 [secretary "1.2.1"]
+                 [secretary "1.2.2"]
                  [slingshot "0.12.2"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
@@ -36,7 +35,7 @@
             [lein-cljfmt "0.1.10"]
             [lein-environ "1.0.0"]
             [lein-expectations "0.0.8"]
-            [lein-figwheel "0.2.2-SNAPSHOT"]]
+            #_[lein-figwheel "0.2.5" :exclusions [org.clojure/tools.nrepl]]]
 
   :cljfmt {:indents {do-template [[:block 1]]
                      context [[:block 1]]
@@ -49,7 +48,7 @@
   ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
 
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [expectations "2.0.16"]
+                                  [expectations "2.1.0"]
                                   [figwheel "0.2.5"]
                                   [figwheel-sidecar "0.2.5"]
                                   [weasel "0.6.0"]
@@ -75,7 +74,6 @@
                                             {:source-paths ["src/prod/cljs"]
                                              :compiler
                                              {:optimizations :advanced
-                                              :cache-analysis false
                                               :pretty-print false}}}}}}
 
   :cljsbuild {:test-commands {"node" ["node" :node-runner "resources/public/js/app.js"]}
