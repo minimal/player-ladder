a8c53f4d3b29fb7f2a5f91a506146ea3ad90e6c0
Chris McDevitt <christophermcdevitt@gmail.com>
2015-06-17 22:01:12 +0100

    use correct ranking for slack message

--- src/clj/ladder/events.clj
+++ src/clj/ladder/events.clj
@@ -7,15 +7,19 @@
             [slingshot.slingshot :refer [try+]]))
 
 
-(defn format-league-bound-players [[best1 best2] [worst1 worst2] league size]
+(defn format-league-bound-players [[best1 best2] [worst1 worst2] league]
   (format (str/join "\n" ["\t\tLeague %s:"
                           "\t\tTop players:"
-                          "\t\t\t\t1.- %s"
-                          "\t\t\t\t2.- %s"
+                          "\t\t\t\t%s.- %s"
+                          "\t\t\t\t%s.- %s"
                           "\t\tBottom players:"
                           "\t\t\t\t%s.- %s"
                           "\t\t\t\t%s.- %s"])
-          (name league) best1 best2 (dec size) worst1 size worst2))
+          (name league)
+          (:rank best1) (:team best1)
+          (:rank best2) (:team best2)
+          (:rank worst1) (:team worst1)
+          (:rank worst2) (:team worst2)))
 
 (defn post-to-slack
   "Post a params map to slack. Returns a channel or nil if noop"
@@ -36,9 +40,9 @@
                                    slack-url]
   (let [match-text (format "%s wins against %s in league %s: %s - %s"
                            winner loser (name league) winner-score loser-score)
-        bound-text (format-league-bound-players (map :team rankings)
-                                                (map :team (take-last 2 rankings))
-                                                league (count rankings))
+        bound-text (format-league-bound-players (map #(select-keys % [:team :rank]) rankings)
+                                                (map #(select-keys % [:team :rank]) (take-last 2 rankings))
+                                                league)
         params {:text (str/join "\n\n" [match-text bound-text])}]
     (post-to-slack slack-url params)))
 
