2db287147c8c4f3d2a9c12eb88e226e65ea983c3
Chris McDevitt <christophermcdevitt@gmail.com>
2016-05-21 16:48:38 +0100

    Next (#14)
    * fix: update deps, fix figwheel/cljs setup
    
    Update readme. Remove old figwheel junks.
    
    * fixup

--- dev/cljs/main.cljs
+++ dev/cljs/main.cljs
@@ -3,7 +3,7 @@
             [figwheel.client :as fw :include-macros true]
             [ladder.utils :refer [guid] :refer-macros [logm]]
             ;; [devtools.core :as devtools]
-            [weasel.repl :as weasel]))
+            ))
 
 ;; (devtools/install!)
 
@@ -14,15 +14,11 @@
     ;; (s/with-fn-validation)
     (app/run-top-level)))
 
+(defn before-load-hook [files]
+  (reset! app/last-hash (app/get-hash))
+  (logm app/last-hash)
+  (fw/default-before-load files))
+
 (enable-console-print!)
-(fw/watch-and-reload
- :websocket-url   "ws://localhost:3449/figwheel-ws"
- :before-jsload (fn [files]
-                  (reset! app/last-hash (app/get-hash))
-                  (logm app/last-hash)
-                  (fw/default-before-load files))
- :on-jsload (fn []
-              (run-refresh)))
-(weasel/connect "ws://localhost:9001" :verbose true :print #{:repl :console})
 
 (app/run-top-level)
--- project.clj
+++ project.clj
@@ -2,30 +2,29 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :source-paths ["src/clj" "target/classes"]
-  :dependencies [[clj-time "0.10.0"]
+  :source-paths ["src/clj"]
+  :dependencies [[clj-time "0.11.0"]
                  [clj-http "2.0.0"]
-                 [cljs-http "0.1.36"]
-                 [potemkin "0.4.1"]
-                 [com.stuartsierra/component "0.2.3"]
-                 [compojure "1.4.0"]
+                 [cljs-http "0.1.40"]
+                 [potemkin "0.4.3"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [compojure "1.5.0"]
                  [buddy/buddy-auth "0.5.3"]
-                 [environ "1.0.0"]
+                 [environ "1.0.3"]
                  [metosin/compojure-api "0.21.0"]
                  [org.omcljs/om "0.8.8"]
-                 [org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.58" :classifier "aot" :exclusions
-                  [org.clojure/tools.reader org.clojure/data.json]]
-                 [org.clojure/tools.reader "0.9.2" :classifier "aot"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [org.clojure/tools.reader "0.10.0" :classifier "aot"]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [prismatic/om-tools "0.3.11"]
-                 [prismatic/schema "0.4.3"]
-                 [prone "0.8.2"]
+                 [org.clojure/core.async "0.2.374"]
+                 [prismatic/om-tools "0.4.0"]
+                 [prismatic/schema "1.1.1"]
+                 [prone "1.1.1"]
                  [ring "1.4.0"]
                  [metosin/ring-middleware-format "0.6.0"]
                  [ring/ring-core "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-defaults "0.2.0"]
                  [sablono "0.3.5"]
                  [secretary "1.2.3"]
                  [slingshot "0.12.2"]
@@ -36,18 +35,16 @@
 
                  [org.clojure/core.match "0.3.0-alpha4"]]
 
+  #_:repositories #_[["sonatype-oss-public"
+                      "https://oss.sonatype.org/content/groups/public/"]]
   :aliases {"test" ["expectations"]}
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-cljfmt "0.1.10"]
-            [lein-environ "1.0.0"]
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-cljfmt "0.5.3"]
+            [lein-environ "1.0.3"]
+            [venantius/yagni "0.1.4"]
             [lein-expectations "0.0.8"]
-            [lein-autoexpect "1.5.0"]
-            [lein-figwheel "0.3.3" :exclusions [org.clojure/tools.nrepl
-                                                org.clojure/clojure
-                                                cider/cider-nrepl
-                                                org.codehaus.plexus/plexus-utils]]
-            ]
-
+            [lein-autoexpect "1.9.0"]
+            [lein-figwheel "0.5.3-2"]]
   :cljfmt {:indents {do-template [[:block 1]]
                      context [[:block 1]]
                      swaggered [[:block 1]]
@@ -56,29 +53,26 @@
                                     "resources/public/js/out"
                                     "out"
                                     :target-path]
-  ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-
+  ;;  ≠ ≡ ≢ ≣ ≤ ≥ ≦ ≧ ≨ ≩ ≪ ≫ ≬ ≭ ≮ ≯
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [expectations "2.0.16"]
-                                  [weasel "0.7.0" :exclusions [org.clojure/clojurescript]]
-                                  [figwheel-sidecar "0.3.7"
-                                   :exclusions [org.clojure/clojurescript]]
-                                  [figwheel "0.3.7"
-                                   :exclusions [org.clojure/clojurescript]]
-                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"
-                                   :exclusions [org.clojure/clojurescript]]
+                                  [expectations "2.1.8"]
+                                  [figwheel-sidecar "0.5.3-2"]
+                                  [figwheel "0.5.3-2"]
+                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
                                   ;; [omdev "0.1.3-SNAPSHOT"]
-                                  [spellhouse/clairvoyant "0.1.0-SNAPSHOT"]
+                                  ;;[spellhouse/clairvoyant "0.1.0-SNAPSHOT"]
                                   [org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [org.clojure/tools.nrepl "0.2.12"]
                                   [reloaded.repl "0.1.0"]
+                                  [clojurescript-build "0.1.9"]
                                   [ring-mock "0.1.5"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
-                   :figwheel {:http-server-root "public" ;; resources/public
-                              :port 3449
+                   :cljsbuild {:builds {:app
+                                        {:source-paths ["dev/cljs" "src/clj"]}}}
+                   :figwheel {:http-server-root "public"
+                              :nrepl-port 7888
                               :css-dirs ["resources/public/css"]}}
 
              :uberjar {:aot [ladder.system]
@@ -93,13 +87,14 @@
 
   :cljsbuild {:test-commands {"node" ["node" :node-runner "resources/public/js/app.js"]}
               :builds {:app {:source-paths ["src/clj"]
+                             :figwheel {:on-jsload "ladder.main/run-refresh"
+                                        :before-jsload "ladder.main/before-load-hook"}
                              :compiler {:output-to "resources/public/js/app.js"
                                         :output-dir "resources/public/js/out"
-                                        :source-map "resources/public/js/out.js.map"
+                                        :source-map true
                                         :externs ["react/externs/react.js"]
                                         :optimizations :none
                                         :main "ladder.main"
+                                        :asset-path "js/out"
                                         :cache-analysis true
-                                        :pretty-print  true}}}}
-
-  )
+                                        :pretty-print  true}}}})
